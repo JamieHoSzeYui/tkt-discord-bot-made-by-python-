b4e61aecd860cffd87902306ddeee28c83d0f2e5
tkt0506 <46648701+tkt0506@users.noreply.github.com>
2020-07-21 17:34:25 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,5 +7,12 @@ bot = commands.Bot(command_prefix='[')
 async def on_ready():
     print(">> Bot is online <<")
    
+@bot.event
+async def on_member_join(member):
+    print(f"{member} join my server! omg , is that even possible (just kidding)")
+
+@bot.event
+async def on_member_remove(member):
+    print(f"{member} leavemy server... QAQ , this is so sad")    
 
 bot.run('NzM1MDU0MDk5NDAxMTQ2NDMw.Xxaqng.O5VULPinfNbInF2y2K8tuEgDXb8')   
