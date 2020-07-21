2f344c6fceae22dc8677334a20a7cc9068b67a86
tkt0506 <46648701+tkt0506@users.noreply.github.com>
2020-07-21 18:13:08 +0800

    ping

--- bot.py
+++ bot.py
@@ -15,8 +15,16 @@ async def on_member_join(member):
 
 @bot.event
 async def on_member_remove(member):
-    print(f"{member} leavemy server... QAQ , this is so sad")    
+    print(f"{member} leave my server... QAQ , this is so sad")    
     channel = bot.get_channel(710883854680064064)
-    await channel.send(f"{member} leavemy server... QAQ , this is so sad")
-    
+    await channel.send(f"{member} leav emy server... QAQ , this is so sad")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"{bot.latency*1000}(ms)")
+
+@bot.command()
+async def round_ping(ctx):
+    await ctx.send(f"{round(bot.latency*1000)}(ms)")
+
 bot.run('NzM1MDU0MDk5NDAxMTQ2NDMw.Xxa4Rw.JUek-FXhs9cYi0sbpGvqY_M1Wrc')   
