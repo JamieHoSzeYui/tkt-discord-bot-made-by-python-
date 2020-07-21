9d607c4c3d8a3425d27d6c9b53844dd065b71b3d
tkt0506 <46648701+tkt0506@users.noreply.github.com>
2020-07-21 17:25:43 +0800

    2

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
-hello
+import discord 
+from discord.ext import commands 
+
+bot = commands.Bot(command_prefix='[')
+
+@bot.event
+async def on_ready():
+    print(">> Bot is online <<")
+   
+
+bot.run('NzM1MDU0MDk5NDAxMTQ2NDMw.Xxaqng.O5VULPinfNbInF2y2K8tuEgDXb8')   
